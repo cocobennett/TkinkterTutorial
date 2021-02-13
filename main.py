fd33cf61ff6286b78f37c636b27e24d4232a29e8
cocobennett <kittenishkoko@gmail.com>
2021-02-13 16:45:24 -0600

    Part Two

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from tkinter import *
+import tkinter as Tk
+import tkinter.ttk as Ttk
 
-window = Tk()
+window = Tk.Tk()
 
 window.title("Welcome")
 
 window.geometry('350x200')
 
-lbl = Label(window, text="Hello World")
+combo = Ttk.Combobox(window)
+combo['values']= (1, 2, 3, 4, 5, "Text")
+combo.current(1) #set the selected item
+combo.grid(column=1, row=1)
 
-lbl.grid(column=0, row=0)
+lbl = Tk.Label(window, text="Hello World")
+
+lbl.grid(column=1, row=0)
+
+input = Tk.Entry(window, width = 10)
+
+input.grid(column=0, row=1)
+
+input.focus()
 
 def clicked():
+    res = f"Welcome to {input.get()}"
+    lbl.configure(text=res)
 
-    lbl.configure(text="Button clicked!")
+btn = Tk.Button(window, text="Click Me", command=clicked, bg="Magenta", fg="White")
 
-btn = Button(window, text="Click Me", command=clicked, bg="Magenta", fg="White")
-
-btn.grid(column=0, row=1)
+btn.grid(column=0, row=0)
 
 window.mainloop()
