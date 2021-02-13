f7a2c86c1def43e42f0a6fc118b0c5dcf18463a0
cocobennett <kittenishkoko@gmail.com>
2021-02-13 14:53:25 -0600

    First part of tutorial done

--- main.py
+++ main.py
@@ -4,4 +4,18 @@ window = Tk()
 
 window.title("Welcome")
 
+window.geometry('350x200')
+
+lbl = Label(window, text="Hello World")
+
+lbl.grid(column=0, row=0)
+
+def clicked():
+
+    lbl.configure(text="Button clicked!")
+
+btn = Button(window, text="Click Me", command=clicked, bg="Magenta", fg="White")
+
+btn.grid(column=0, row=1)
+
 window.mainloop()
